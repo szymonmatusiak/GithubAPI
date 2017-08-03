f954bf08b0c53ff53b3518414de0b6947d04c04e
Szymon Matusiak <szymon.matusiak1@gmail.com>
2017-08-03 21:15:44 +0200

    added base package

---
+++ app/src/main/java/com/example/szymon/githubapi/base/IBasePresenter.java
@@ -0,0 +1,19 @@
+package com.example.szymon.githubapi.base;
+
+public interface IBasePresenter<V extends IBaseView> {
+
+    /**
+     * Attach view.
+     *
+     * @param view the view
+     */
+    void attachView(V view);
+
+    /**
+     * Detach view.
+     *
+     * @param retainInstance the retain instance
+     */
+    void detachView(boolean retainInstance);
+
+}
