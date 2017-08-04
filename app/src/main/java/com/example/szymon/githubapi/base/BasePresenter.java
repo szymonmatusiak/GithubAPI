d4036ed361db738d6cda1db35da8937f5ca74a9b
Szymon Matusiak <szymon.matusiak1@gmail.com>
2017-08-04 12:52:29 +0200

    added base package

---
+++ app/src/main/java/com/example/szymon/githubapi/base/BasePresenter.java
@@ -0,0 +1,39 @@
+package com.example.szymon.githubapi.base;
+
+import java.lang.ref.WeakReference;
+
+public abstract class BasePresenter<V extends IBaseView> implements IBasePresenter<V> {
+
+    private WeakReference<V> viewReference;
+
+    @Override
+    public void attachView(V view) {
+        viewReference = new WeakReference<>(view);
+    }
+
+    /**
+     * Gets view.
+     *
+     * @return the view
+     */
+    protected V getView() {
+        return viewReference.get();
+    }
+
+    /**
+     * Is view attached boolean.
+     *
+     * @return the boolean
+     */
+    protected boolean isViewAttached() {
+        return viewReference != null && viewReference.get() != null;
+    }
+
+    @Override
+    public void detachView(boolean retainInstance) {
+        if (viewReference != null) {
+            viewReference.clear();
+            viewReference = null;
+        }
+    }
+}
